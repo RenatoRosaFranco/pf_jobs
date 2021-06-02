8f1de91ee3bc118c0624095b8e3a64ab6c9a1995
Renato Rosa Franco <renato_ny@live.com>
2021-06-02 01:15:58 -0300

    Updating project structure and installed missing gems aka (Rspec, FastJsonApi)

--- config/routes.rb
+++ config/routes.rb
@@ -9,17 +9,39 @@ Rails.application.routes.draw do
   # Application
   # @implemented
   root to: 'home#index'
-  resources :jobs, only: [:index, :show]
-  resources :companies, only: [:index, :show]
-  resources :courses, only: [:index]
+  resources :jobs, only: %i[index show]
+  resources :companies, only: %i[index show]
+  resources :courses, only: %i[index]
 
   # Dashboard
   # @implemented
   namespace :dashboard do
     get '', to: 'home#index'
     resources :jobs
-    resources :invoices, only: [:index]
-    resources :profiles, only: [:edit, :update]
-    resources :accounts, only: [:edit, :update, :destroy]
+    resources :invoices, only: %i[index]
+    resources :profiles, only: %i[edit update]
+    resources :accounts, only: %i[edit update destroy]
+  end
+
+  # Blog => https://blog.application.com.br
+  # @implemented
+  namespace :blog, constraint: { subdomain: 'blog' }, path: '/' do
+    # some code here
+  end
+
+  # API => https://api.application.com.br/
+  # @implemented
+  namespace :api, constraint: { subdomain: 'api' }, path: '/' do
+    namespace :v1 do
+      defaults format: :json do
+        # some json code from here
+      end
+    end
+
+    namespace :v2 do
+      defaults format: :xml do
+        # some xml code from here
+      end
+    end
   end
 end
