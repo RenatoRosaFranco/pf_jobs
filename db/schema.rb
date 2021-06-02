e0f07b9e65b04279cb4f53a2d98162460378a22d
Renato Rosa Franco <renato_ny@live.com>
2021-06-02 09:20:01 -0300

    Cloudinary and PaperTrail added to the project.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_02_042420) do
+ActiveRecord::Schema.define(version: 2021_06_02_121849) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -146,6 +146,16 @@ ActiveRecord::Schema.define(version: 2021_06_02_042420) do
     t.string "cpf_or_cnpj"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string "item_type", null: false
+    t.integer "item_id", limit: 8, null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.text "object", limit: 1073741823
+    t.datetime "created_at"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
 end
